10960d995bc2180d4e7603193b1a608a3254c1cb
Laurie Roy <laurieroy.dev@gmail.com>
2023-06-30 23:22:47 -0500

    Add Navbar -add font, CSS -Link -types

---
+++ notes.md
@@ -0,0 +1,6 @@
+Adding in taking notes. 
+
+Had to fix path and eslint etc config, so coming back to this build after a week or so.
+
+7/10
+relistening to everything already
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { useState } from "react";
+import Navbar from "@/scenes/navbar";
+import { SelectedPage } from "@/shared/types";
+
 function App() {
-  return <div className="app text-gray-10">app</div>
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+
+  return (
+    <div className="app bg-gray-20">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+    </div>
+  );
 }
 
 export default App;
