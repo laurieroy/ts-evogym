bc5d64be00c9de279f958576d7ccfc9ea770a47a
Laurie Roy <laurieroy.dev@gmail.com>
2023-07-12 13:12:53 -0500

    Add Our Classes Section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { SelectedPage } from "@/shared/types";
 import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits";
 import Navbar from "@/scenes/navbar";
+import OurClasses from "./scenes/ourClasses";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -33,6 +34,7 @@ function App() {
       />
       <Home setSelectedPage={setSelectedPage} />
       <Benefits setSelectedPage={setSelectedPage} />
+      <OurClasses setSelectedPage={setSelectedPage} />
     </div>
   );
 }
